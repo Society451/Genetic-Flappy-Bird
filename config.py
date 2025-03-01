64a61f591c83f29243d3a14430ab867e5a9d5d3b
S451 <paul257@ohs.stanford.edu>
2025-02-28 20:03:34 -0600

    changes

--- config.py
+++ config.py
@@ -9,14 +9,3 @@ PIPE_FREQUENCY = 50  # New pipe every N frames
 
 # Bird settings
 BIRD_COUNT = 15      # Number of birds in population
-
-# Speed settings
-MIN_SPEED = 1
-MAX_SPEED = 100
-DEFAULT_SPEED = 1
-
-# Visualization settings
-DEFAULT_SHOW_VECTORS = True
-DEFAULT_SHOW_OBSTACLE_LINES = True
-VECTOR_COLOR = (255, 0, 0)  # Red
-OBSTACLE_LINE_COLOR = (0, 0, 255)  # Blue
